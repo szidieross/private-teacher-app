f9d7e80754c5db00a855304daede6db98091c3f9
szidieross <szidieross@gmail.com>
2024-05-02 01:32:21 +0300

    added profile page

--- app/(client)/(routes)/profile/page.tsx
+++ app/(client)/(routes)/profile/page.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
-    return (
-      <main>
-        my profile page
-      </main>
-    );
+import { changePremium, changeUsername, getSession } from "@/app/actions";
+import { redirect } from "next/navigation";
+
+const ProfilePage = async () => {
+  const session = await getSession();
+
+  if(!session.isLoggedIn){
+    redirect("/")
   }
-  
+
+  return (
+    <div className="profile">
+      <h1>Welcome to the ProfilePage</h1>
+      <p>
+        Welcome, <b>{session.username}</b>
+      </p>
+      <span>You are a <b>{session.isPro ? "Premium" : "Free"}</b> user</span>
+      <form action={changePremium}>
+        <button>{session.isPro ? "Cancel" : "Buy"} Premium</button>
+      </form>
+
+      <form action={changeUsername}>
+        <input type="text" name="username" required placeholder={session.username} />
+        <button>Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfilePage;
