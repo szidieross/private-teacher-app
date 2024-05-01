a00917ab0fdf3a110526bc2b8e9a619a18b4c449
szidieross <szidieross@gmail.com>
2024-05-01 15:43:23 +0300

    added php endpoint for users and axios

--- app/api/services/user.service.ts
+++ app/api/services/user.service.ts
@@ -2,43 +2,61 @@ import pool from "@/app/libs/mysql";
 import { SimpleUserModel, UserModel } from "../models/user.model";
 import { SimpleUserDto, UserDto } from "../dtos/user.dto";
 import { toSimpleUserModel, toUserModel } from "../mappers/user.mapper";
+import axios from 'axios';
 
-export const getUsers = async (): Promise<UserModel[]> => {
-  try {
-    const db = await pool.getConnection();
-    const query = "SELECT * FROM users";
-    const [rows] = await db.execute(query);
-    db.release();
-
-    if (!Array.isArray(rows)) {
-      throw new Error("Query result is not an array");
+// Elküldi a GET kérést a PHP végpontra, amely visszaadja az összes felhasználót
+// const fetchUsers = async () => {
+  export const getUsers = async (): Promise<UserModel[]> => {
+    try {
+        const response = await axios.get('http://localhost/private-teacher-app/private-teacher-app-server/users.php');
+        const users = response.data;
+        console.log('Users:', users);
+        return users;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return [];
     }
-
-    const data: UserDto[] = (rows as any).map((row: any) => {
-      return {
-        user_id: row.user_id,
-        username: row.username,
-        password: row.password,
-        email: row.email,
-        phone: row.phone,
-        profile_picture: row.profile_picture,
-        created_at: row.created_at,
-        first_name: row.first_name,
-        last_name: row.last_name,
-        role: row.role,
-      };
-    });
-
-    const users: UserModel[] = data.map((row: UserDto) => {
-      return toUserModel(row);
-    });
-
-    return users;
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return [];
-  }
 };
+
+// fetchUsers();
+
+
+// export const getUsers = async (): Promise<UserModel[]> => {
+//   try {
+//     const db = await pool.getConnection();
+//     const query = "SELECT * FROM users";
+//     const [rows] = await db.execute(query);
+//     db.release();
+
+//     if (!Array.isArray(rows)) {
+//       throw new Error("Query result is not an array");
+//     }
+
+//     const data: UserDto[] = (rows as any).map((row: any) => {
+//       return {
+//         user_id: row.user_id,
+//         username: row.username,
+//         password: row.password,
+//         email: row.email,
+//         phone: row.phone,
+//         profile_picture: row.profile_picture,
+//         created_at: row.created_at,
+//         first_name: row.first_name,
+//         last_name: row.last_name,
+//         role: row.role,
+//       };
+//     });
+
+//     const users: UserModel[] = data.map((row: UserDto) => {
+//       return toUserModel(row);
+//     });
+
+//     return users;
+//   } catch (error) {
+//     console.error("Error fetching users:", error);
+//     return [];
+//   }
+// };
 
 
 export const getUserById = async (userId: number): Promise<UserModel> => {
