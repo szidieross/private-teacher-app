3362dd1f1416d8289dbb8f9f4ec569de063d605c
szidieross <szidieross@gmail.com>
2024-05-19 16:54:16 +0300

    image fixed

--- app/(client)/(routes)/profile/appointments/page.tsx
+++ app/(client)/(routes)/profile/appointments/page.tsx
@@ -13,7 +13,6 @@ export default async function Home() {
 
   return (
     <main>
-      appointments will go here
       {session.role === "teacher" && session.userId ? (
         <TeacherAppointments userId={session.userId} />
       ) : (
