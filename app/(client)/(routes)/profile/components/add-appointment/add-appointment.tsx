de28c10634cf213585a03a2d7a7e297a8a8d7130
szidieross <szidieross@gmail.com>
2024-05-20 10:35:34 +0300

    styling

--- app/(client)/(routes)/profile/components/add-appointment/add-appointment.tsx
+++ app/(client)/(routes)/profile/components/add-appointment/add-appointment.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { FC, useEffect, useState } from "react";
 import {
   Container,
@@ -5,11 +7,15 @@ import {
   Button,
   Grid,
   Typography,
-  Paper,
+  Card,
+  Box,
 } from "@mui/material";
 import useAppointmentsService from "@/app/(client)/services/appointment.service";
 import useLessonsService from "@/app/(client)/services/lesson.service";
 import { LessonModel } from "@/app/api/models/lesson.model";
+import EditCalendarIcon from "@mui/icons-material/EditCalendar";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import { colors } from "@/app/(client)/constants/color.constant";
 
 type Props = {
   teacherId: number;
@@ -18,27 +24,7 @@ type Props = {
 const AddAppointment: FC<Props> = ({ teacherId }) => {
   const { createAppointment } = useAppointmentsService();
   const { getLessonsByTeacherId } = useLessonsService();
-  const [formData, setFormData] = useState({
-    date: "",
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const selectedDate = new Date(formData.date);
-    createAppointment(teacherId, selectedDate);
-    setFormData({
-      date: "",
-    });
-  };
-
+  const [formData, setFormData] = useState({ date: "" });
   const [lessons, setLessons] = useState<LessonModel[] | null>(null);
 
   useEffect(() => {
@@ -46,58 +32,95 @@ const AddAppointment: FC<Props> = ({ teacherId }) => {
       try {
         const lessonsData = await getLessonsByTeacherId(teacherId);
         setLessons(lessonsData);
-        console.log("lessonsData", lessonsData);
       } catch (error) {
         console.error("Failed to fetch lessons:", error);
       }
     };
-
     fetchLessons();
   }, [teacherId, getLessonsByTeacherId]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createAppointment(teacherId, new Date(formData.date));
+    setFormData({ date: "" });
+  };
 
   return (
     <Container maxWidth="sm">
       {lessons?.length === 0 ? (
-        <Typography>
-          You need to add a lesson to create appointments.
-        </Typography>
-      ) : (
-        <Paper style={{ padding: 20, marginBottom: 20 }} elevation={3}>
-          <Typography variant="h5" gutterBottom>
-            Add a time to your appointment
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          // height="100vh"
+          flexDirection="column"
+        >
+          <Typography variant="h6" color="textSecondary" align="center">
+            No lessons available. Add a lesson to create appointments.
           </Typography>
-          <form
-            style={{ width: "100%", marginTop: 10 }}
-            onSubmit={handleSubmit}
+        </Box>
+      ) : (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          // height="100vh"
+          bgcolor={colors.background}
+          padding={2}
+        >
+          <Card
+            sx={{
+              padding: 4,
+              borderRadius: 3,
+              maxWidth: 500,
+              width: "100%",
+              boxShadow: 5,
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  label="Date"
-                  type="datetime-local"
-                  name="date"
-                  value={formData.date}
-                  onChange={handleChange}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                />
+            <Typography variant="h5" align="center" gutterBottom>
+              Schedule an Appointment
+            </Typography>
+            <form onSubmit={handleSubmit} style={{ marginTop: 20 }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Date and Time"
+                    type="datetime-local"
+                    name="date"
+                    value={formData.date}
+                    onChange={handleChange}
+                    InputLabelProps={{ shrink: true }}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    endIcon={<EditCalendarIcon />}
+                    sx={{
+                      paddingY: 1.5,
+                      backgroundColor: colors.primary,
+                      "&:hover": {
+                        backgroundColor: colors.background,
+                      },
+                    }}
+                  >
+                    Create Appointment
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: 20 }}
-                  fullWidth
-                >
-                  Create Appointment
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Paper>
+            </form>
+          </Card>
+        </Box>
       )}
     </Container>
   );
