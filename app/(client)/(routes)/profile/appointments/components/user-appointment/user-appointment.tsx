d1645bf5c6cd2c70b4719c277030ebb9dc54078d
szidieross <szidieross@gmail.com>
2024-05-20 03:11:56 +0300

    style fix

--- app/(client)/(routes)/profile/appointments/components/user-appointment/user-appointment.tsx
+++ app/(client)/(routes)/profile/appointments/components/user-appointment/user-appointment.tsx
@@ -1,27 +1,50 @@
 "use client";
 
 import React, { FC, useEffect, useState } from "react";
-import { Box, Button, IconButton, Link, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Link,
+  Typography,
+  Snackbar,
+  Alert,
+  Paper,
+} from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import useAppointmentsService from "@/app/(client)/services/appointment.service";
-import { AppointmentModel } from "@/app/api/models/appointment.model";
 import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import { AppointmentModel } from "@/app/api/models/appointment.model";
+import "./user-appointment";
+import { colors } from "@/app/(client)/constants/color.constant";
 
 type Props = {
   userId: number;
 };
 
 const UserAppointments: FC<Props> = ({ userId }) => {
-  const { getAppointmentByUserId, cancelAppointment, deleteAppointment } =
+  const { getAppointmentByUserId, cancelAppointment } =
     useAppointmentsService();
   const [appointments, setAppointments] = useState<AppointmentModel[] | null>(
     null
   );
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const handleCancel = async (appointmentId: number) => {
-    await cancelAppointment(appointmentId);
-    const updatedAppointments = await getAppointmentByUserId(userId);
-    setAppointments(updatedAppointments);
+    try {
+      await cancelAppointment(appointmentId);
+      const updatedAppointments = await getAppointmentByUserId(userId);
+      setAppointments(updatedAppointments);
+      setSnackbarMessage("Appointment successfully canceled.");
+    } catch (error) {
+      setSnackbarMessage("Error canceling appointment.");
+      console.error("Error canceling appointment:", error);
+    }
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
   };
 
   useEffect(() => {
@@ -37,6 +60,22 @@ const UserAppointments: FC<Props> = ({ userId }) => {
     fetchData();
   }, [getAppointmentByUserId, userId]);
 
+  const formatDate = (date: string) => {
+    const dateObj = new Date(date);
+    return (
+      dateObj.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }) +
+      " " +
+      dateObj.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+    );
+  };
+
   const columns: GridColDef[] = [
     {
       field: "id",
@@ -44,43 +83,28 @@ const UserAppointments: FC<Props> = ({ userId }) => {
       width: 90,
       editable: false,
     },
-    {
-      field: "subject",
-      headerName: "Subject",
-      width: 110,
-      editable: false,
-    },
+    { field: "subject", headerName: "Subject", width: 180 },
     {
       field: "name",
       headerName: "Teacher",
-      width: 150,
-      editable: false,
+      width: 200,
       renderCell: (params) => (
-        <Link href={`/teachers/${params.row.teacherId}`}>
+        <Link
+          href={`/teachers/${params.row.teacherId}`}
+          underline="hover"
+          color="primary"
+        >
           {`${params.row.name}`}
         </Link>
       ),
     },
-    {
-      field: "date",
-      headerName: "Date",
-      width: 150,
-      editable: false,
-    },
+    { field: "date", headerName: "Date", width: 200 },
     {
       field: "action",
       headerName: "Action",
       sortable: false,
-      width: 160,
-      editable: false,
+      width: 120,
       renderCell: (params) => (
-        // <Button
-        //   variant="text"
-        //   color="error"
-        //   onClick={() => handleCancel(params.row.appointmentId)}
-        // >
-        //   Cancel
-        // </Button>
         <IconButton
           color="error"
           onClick={() => handleCancel(params.row.appointmentId)}
@@ -91,38 +115,62 @@ const UserAppointments: FC<Props> = ({ userId }) => {
     },
   ];
 
-  const rows = appointments?.map((item, index) => {
-    return {
+  const rows =
+    appointments?.map((item, index) => ({
       id: index + 1,
       appointmentId: item.appointmentId,
       subject: item.categoryName,
       teacherId: item.teacherId,
       name: `${item.firstName} ${item.lastName}`,
-      // date: "date",
-      date: item.startTime,
-    };
-  });
+      date: formatDate(item.startTime),
+    })) || [];
 
   const handleCellClick = (params: any) => {
     params.event?.stopPropagation(); // Prevent focusing on cells
   };
 
   return (
-    <Box sx={{ p: 2 }}>
-      <Typography variant="h5" sx={{ mb: 2 }}>
+    <Box
+      sx={{ p: 2 }}
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+    >
+      <Typography
+        variant="h5"
+        sx={{ mb: 2, fontWeight: "bold", color: "primary.main" }}
+      >
         Your Appointments
       </Typography>
-      <Box sx={{ height: "fit-content", width: "100%" }}>
-        {rows && (
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            checkboxSelection={false}
-            disableRowSelectionOnClick
-            onCellClick={handleCellClick}
-          />
-        )}
+      <Box>
+        <Paper elevation={2}>
+          {rows && (
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              checkboxSelection={false}
+              disableRowSelectionOnClick
+              onCellClick={handleCellClick}
+              autoHeight
+              autosizeOnMount
+              sx={{ maxWidth: "90vw",backgroundColor:colors.background }}
+            />
+          )}
+        </Paper>
       </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="info"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
--- app/(client)/constants/color.constant.ts
+++ app/(client)/constants/color.constant.ts
@@ -1,6 +1,7 @@
 export const colors = {
   primary: "rgb(161, 161, 216)",
   secondary: "rgb(105, 105, 122)",
+  background:"rgb(240, 245, 252)",
   white: "#ffffff",
   black: "#000000",
 };
