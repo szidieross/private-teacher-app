5ebe11ed7235eef0c6f4a82772cb38d0bcee4132
szidieross <szidieross@gmail.com>
2024-05-26 01:42:56 +0300

    mtadata

--- app/(client)/(routes)/users/[userId]/page.tsx
+++ app/(client)/(routes)/users/[userId]/page.tsx
@@ -1,8 +1,23 @@
+import { getUserById } from "@/app/api/services/user.service";
 import UserItem from "./components/user-item/user-item";
+import { findPageByLabel } from "@/app/(client)/utils/page.util";
 
 type Props = {
   params: { userId: number };
 };
+
+export const generateMetadata = async (props: Props) => {
+  const { params } = props;
+  const user = await getUserById(params.userId)
+  const page = findPageByLabel("teachers");
+
+  return {
+    title: `${user.firstName} ${user.lastName}`,
+    description: page.description,
+    keywords: page.keywords,
+  };
+};
+
 export default function Home(props: Props) {
   return (
     <main>
--- app/(client)/(routes)/users/page.tsx
+++ app/(client)/(routes)/users/page.tsx
@@ -1,6 +1,17 @@
 import { isLoggedIn } from "@/app/actions";
 import UserList from "./components/user-list/UserList";
 import { redirect } from "next/navigation";
+import { findPageByLabel } from "../../utils/page.util";
+
+export const generateMetadata = async () => {
+  const page = findPageByLabel("users");
+
+  return {
+    title: page.title,
+    description: page.description,
+    keywords: page.keywords,
+  };
+};
 
 export default async function Home() {
   const isSession = await isLoggedIn();
